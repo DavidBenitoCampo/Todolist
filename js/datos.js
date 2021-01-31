9296c2d22e76c2c49e9255012c366c140156f656
DavidBenitoCampo <davbecam@gmail.com>
2021-01-31 23:13:53 +0100

    bootstrap agregar tareas

--- js/datos.js
+++ js/datos.js
@@ -1,6 +1,1 @@
-var taskList = new Array();
-taskList = [{ 'idTarea': 0, 'titulo': 'Estudiar Javascript', 'prioridad': 'urgente' },
-{ 'idTarea': 1, 'titulo': 'dormir', 'prioridad': 'diaria' },
-{ 'idTarea': 2, 'titulo': 'Salir a comer', 'prioridad': 'mensual' }
 
-];
--- js/funciones.js
+++ js/funciones.js
@@ -1,72 +1,90 @@
-const formulario = document.getElementById('formulario');
-const input = document.getElementById('input');
-const taskList = document.getElementById('lista-tareas')
+const formulario = document.getElementById('formulario')
+const listaTareas = document.getElementById('lista-tareas')
 const template = document.getElementById('template').content
 const fragment = document.createDocumentFragment()
+let tareas = {}
 
-
-let tareas = {
-    0: {
-        id: 0,
-        texto: 'Estudiar Javascript',
-        prioridad: 'urgente',
-        estado: false,
-    },
-
-    1: {
-        id: 1,
-        texto: 'dormir',
-        prioridad: 'diaria',
-        estado: false,
-    },
-
-    2: {
-        id: 2,
-        texto: 'Salir a comer',
-        prioridad: 'mensual',
-        estado: false,
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('tareas')) {
+        tareas = JSON.parse(localStorage.getItem('tareas'))
     }
+    pintarTareas()
+})
 
-};
+listaTareas.addEventListener('click', (e) => { btnAccion(e) })
 
-formulario.addEventListener('submit', evento => {
-    evento.preventDefault()
+formulario.addEventListener('submit', e => {
+    e.preventDefault()
+    // console.log(e.target[0].value)
+    // console.log(e.target.querySelector('input').value)
+    setTarea(e)
+})
 
-    console.log(input.value)
+const setTarea = e => {
+    const texto = e.target.querySelector('input').value
 
-    setTarea(evento)
-});
-
-const setTarea = evento => {
-    if (input.value.trim() === '') {
-        console.log('esta vacio')
+    if (texto.trim() === '') {
+        console.log('está vacio')
         return
-    };
-
-
-    const tarea =
-    {
-
+    }
+    const tarea = {
         id: Date.now(),
-        texto: input.value,
-        prioridad: 'urgente',
-        estado: false,
-
+        texto: texto,
+        estado: false
     }
 
     tareas[tarea.id] = tarea
-
-
-    // console.log('tarea')
-    formulario.reset();
-    input.focus();
-
     pintarTareas()
+
+    formulario.reset()
+    e.target.querySelector('input').focus()
 }
 
 const pintarTareas = () => {
-    Object.values(tareas).forEach(tarea => {
-        console.log(tarea)
 
-    });
+    localStorage.setItem('tareas', JSON.stringify(tareas))
+
+    if (Object.values(tareas).length === 0) {
+        listaTareas.innerHTML = `<div class="alert alert-dark text-center">Sin tareas pendientes </div>`
+        return
+    }
+
+    listaTareas.innerHTML = ''
+
+    Object.values(tareas).forEach(tarea => {
+        const clone = template.cloneNode(true)
+        clone.querySelector('p').textContent = tarea.texto
+
+        if (tarea.estado) {
+            clone.querySelectorAll('.fas')[0].classList.replace('fa-check-circle', 'fa-undo-alt')
+            clone.querySelector('.alert').classList.replace('alert-warning', 'alert-primary')
+            clone.querySelector('p').style.textDecoration = 'line-through'
+        }
+
+        clone.querySelectorAll('.fas')[0].dataset.id = tarea.id
+        clone.querySelectorAll('.fas')[1].dataset.id = tarea.id
+        fragment.appendChild(clone)
+    })
+    listaTareas.appendChild(fragment)
+}
+
+const btnAccion = e => {
+    // console.log(e.target.classList.contains('fa-check-circle'))
+    if (e.target.classList.contains('fa-check-circle')) {
+        tareas[e.target.dataset.id].estado = true
+        pintarTareas()
+    }
+
+    if (e.target.classList.contains('fa-minus-circle')) {
+        // console.log(e.target.dataset.id)
+        delete tareas[e.target.dataset.id]
+        pintarTareas()
+    }
+
+    if (e.target.classList.contains('fa-undo-alt')) {
+        tareas[e.target.dataset.id].estado = false
+        pintarTareas()
+    }
+
+    e.stopPropagation()
 }
